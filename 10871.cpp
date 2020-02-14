3cd1fc420ce60b72437be183c6e3018691dbac84
panya <kjy2844@kaist.ac.kr>
2020-02-14 11:32:37 +0900

    0214 am

---
+++ 10871.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n, x, a;
+    cin >> n >> x;
+
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        if(a < x) cout << a << " ";
+    }
+
+    return 0;
+}
